97dbcd50ca9d9a48168f453afd7f78218bffbde2
mark213364 <islamgaliev.maksim12@gmail.com>
2026-09-18 20:52:37 +0500

    Update index.js

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ export default {
     ).bind(chatId).first();
 
 await sendMessage(env.BOT_TOKEN, chatId,
-  '👋 Привет! \n\nВыбери действие 👇',
+  '👋 Привет! Запускайся в кликер \n\nВыбери действие 👇',
    {
    inline_keyboard: [[
         {
